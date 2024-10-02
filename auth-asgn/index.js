76238bcc178217788d655cfb2946a1c0af73d0ea
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-10-03 01:15:29 +0530

    done

--- auth-asgn/index.js
+++ auth-asgn/index.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const jwtPassword = "123456";
 
 const app = express();
-
+app.use(express.json())
 const ALL_USERS = [
   {
     username: "harkirat@gmail.com",
@@ -23,8 +23,13 @@ const ALL_USERS = [
 ];
 
 function userExists(username, password) {
-  // write logic to return true or false if this user exists
-  // in ALL_USERS array
+  let userExists = false;
+  for (let i = 0; i<ALL_USERS.length; i++) {
+    if(ALL_USERS[i].username == username && ALL_USERS[i].password == password) {
+        userExists = true;
+    }
+  }
+  return userExists;
 }
 
 app.post("/signin", function (req, res) {
@@ -37,8 +42,25 @@ app.post("/signin", function (req, res) {
     });
   }
 
-  var token = jwt.sign({ username: username }, "shhhhh");
+  var token = jwt.sign({ username: username }, jwtPassword);
   return res.json({
     token,
   });
 });
+
+app.get("/users", function (req, res) {
+  const token = req.headers.authorization;
+  const decoded = jwt.verify(token, jwtPassword);
+  const username = decoded.username;
+  res.json({
+    users: ALL_USERS.filter(function(value) {
+        if (value.username == username) {
+            return false
+        } else {
+            return true;
+        }
+    })
+  })
+});
+
+app.listen(3000)
