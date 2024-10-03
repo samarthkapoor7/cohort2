a2fcb720bbbe5cc81fcb13c48c7b55b17fdac877
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-10-03 18:47:47 +0530

    db-asgn

--- db-asgn/index.js
+++ db-asgn/index.js
@@ -1,13 +1,33 @@
+const express = require("express")
+const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://kapoorsamarth7:wBMJixjgzyOIIhZY@cluster0.ck9mw.mongodb.net/")
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://kapoorsamarth7:wBMJixjgzyOIIhZY@cluster0.ck9mw.mongodb.net/usersappnew")
 
 const User = mongoose.model('Users', { name: String, email: String, password: String });
 
-const user = new User({ 
-    name: 'Samarth Kapoor', 
-    email: "asbf@example.com", 
-    password: '123456'
+app.post("/signup", async function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    const existingUser = await User.findOne({ email: username });
+
+    if (existingUser) {
+        return res.status(400).send("Username already exists");
+    }
+
+    const user = new User({ 
+    name: name, 
+    email: email,
+    password: password
 });
 
 user.save();
+res.json({
+    "msg": "user created successfully"
+})
+
+})
