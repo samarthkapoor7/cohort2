9d123a3ca75da004415aa60616dff0c88b07cce5
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-10-10 01:16:01 +0530

    todo react

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,11 +1,46 @@
-import './App.css'
+
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([{
+    title: "go to gym",
+    description: "go to gym from 7-9",
+    completed: false
+  }, {
+    title: "study DSA",
+    description: "study DSA from 9-10",
+    completed: true
+  }, {
+    title: "study DSA",
+    description: "study DSA from 9-10",
+    completed: true
+  }]);
+
+  function addTodo() {
+    setTodos([...todos, {
+      title: "new Todo",
+      description: "desac of new todo"
+    }])
+  }
+
   return (
     <div>
-      hi there
+      <button onClick={addTodo}>Add a randopm todo</button>
+      {/* <Todo title={todos[0].title}description={todos[0].description} />
+      <Todo title={todos[1].title} description={todos[1].description} /> */}
+      {todos.map(function(todo) {
+        return <Todo title={todo.title} description={todo.description} />
+      })}
     </div>
   )
+}
+
+
+function Todo(props) {
+  return <div>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>
+  </div>
 }
 
 export default App
