894c2ea9f2ff8f452108f4313985dec11bc3548b
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-10-10 18:19:25 +0530

    week-6

---
+++ week-6-experiments/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <>
+      <Header title="samarth1"></Header>
+      <Header title="samarth2"></Header>
+    </>
+  )
+}
+
+function Header({title}) {
+  return <div>
+    {title}
+  </div>
+}
+ 
+export default App
