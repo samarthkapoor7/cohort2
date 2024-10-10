e1865729becb9d56ff188699861d36f7eb3377ad
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-10-11 02:44:49 +0530

    wrapper

--- week-6-experiments/src/App.jsx
+++ week-6-experiments/src/App.jsx
@@ -1,29 +1,19 @@
-import React, { Fragment, useState } from 'react';
 
 function App() {
-  const [title, setTitle] = useState("my name is samarth");
-
-  function updateTitle() {
-    setTitle("my name is " + Math.random());
-  }
-
-  return (
-    <div>
-      <button onClick={updateTitle}>Update the title</button>
-      <Header title={title}></Header>
-      <Header title="samarth2"></Header>
-      <Header title="samarth3"></Header>
-      <Header title="samarth4"></Header>
-      <Header title="samarth5"></Header>
-      <Header title="samarth6"></Header>
-    </div>
-  )
-}
-
-function Header({title}) {
   return <div>
-    {title}
+  <CardWrapper>
+    hi there
+  </CardWrapper>
+  <CardWrapper>
+    hello
+  </CardWrapper>
   </div>
 }
- 
+
+function CardWrapper({children}) {
+  return <div style={{border: "2px solid black", padding: 20}}>
+    {children}
+  </div>
+}
+
 export default App
