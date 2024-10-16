7f96cae82927d13f0c1d58f269c8ccaba72151b9
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-10-17 00:53:40 +0530

    dukaan

---
+++ tailwind-prac/src/stories/components/RevenueCard.stories.jsx
@@ -0,0 +1,21 @@
+import { RevenueCard } from '../../components/RevenueCard';
+
+export default {
+  component: RevenueCard,
+};
+
+export const BigAmount = {
+  args: {
+    orderCount: '123',
+    title: "ASd",
+    amount: "1000000000"
+  },
+};
+
+export const SmallAmount = {
+    args: {
+      orderCount: '123',
+      title: "ASd",
+      amount: "100"
+    },
+  };
