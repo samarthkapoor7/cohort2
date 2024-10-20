1abefc7e4d4c5ff7a11c8636ae6537063767774e
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-10-20 18:13:57 +0530

    array

--- node-app/src/a.ts
+++ node-app/src/a.ts
@@ -1,2 +1,11 @@
-const greet = (name) => `Hello, ${name}!`;
+function maxValue(arr: number[]) {
+    let max = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+    }
+    return max;
+}
 
+console.log(maxValue([1, 2, 3]));
