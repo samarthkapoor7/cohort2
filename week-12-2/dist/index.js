aaa5ef6579492524f618ff95c14ccdaf9066e3e6
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-10-28 19:19:59 +0530

    hard

---
+++ week-12-2/dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+;
+function sumOfAge(user1, user2) {
+    return user1.age + user2.age;
+}
+const age = sumOfAge({
+    name: 'John',
+    age: 20
+}, {
+    name: 'Jane',
+    age: 25
+});
+console.log(age);
