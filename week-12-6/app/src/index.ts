aebb24967df5f1e657b74412d33424a195b62a74
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-11-06 03:59:41 +0530

    connection pool

---
+++ week-12-6/app/src/index.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+export interface Env {
+	DATABASE_URL: string
+}
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+		const prisma = new PrismaClient({
+			datasourceUrl: env.DATABASE_URL,
+		}).$extends(withAccelerate())
+
+		const response = await prisma.log.create({
+			data: {
+				level: 'Info',
+				message: `asdsasdas`,
+				meta: {
+					headers: JSON.stringify(request.headers),
+				},
+			},
+		})
+		  console.log(JSON.stringify(response))
+
+		  return  Response.json(response);
+	},
+}
