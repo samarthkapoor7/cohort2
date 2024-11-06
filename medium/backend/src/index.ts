383fcc1ed467308823c68c9e616a659cc7810023
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-11-07 03:23:32 +0530

    mw

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,30 +1,79 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign, verify } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET: string,
+  }
+}>()
 
-app.post('/api/vi/signup', (c) => {
-  return c.text('signup route')
+app.use('/api/v1/blog/*', async (c, next) => {
+  const header = c.req.header("authorization") || "";
+  const token = header.split(" ")[1]
+  const response = await verify(token, c.env.JWT_SECRET)
+  if(response.id) {
+    next()
+  } else {
+    c.status(403)
+    return c.json({ error: "unauthorized" })
+  }
 })
 
-app.post('/api/vi/signin', (c) => {
-  return c.text('signin route')
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+  datasourceUrl: c.env?.DATABASE_URL,
+}).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password
+    }
+  });
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt });
 })
 
-app.post('/api/vi/blog', (c) => {
+app.post('/api/v1/signin', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	const user = await prisma.user.findUnique({
+		where: {
+			email: body.email
+		}
+	});
+
+	if (!user) {
+		c.status(403);
+		return c.json({ error: "user not found" });
+	}
+
+	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+	return c.json({ jwt });
+})
+
+app.post('/api/v1/blog', (c) => {
   return c.text('post blog route')
 })
 
-app.put('/api/vi/blog', (c) => {
+app.put('/api/v1/blog', (c) => {
   return c.text('post blog route')
 })
 
-app.get('/api/vi/blog/:id', (c) => {
+app.get('/api/v1/blog/:id', (c) => {
   const id = c.req.param('id')
   console.log(id);
   return c.text('get blog route')
 })
 
-app.get('/api/vi/blog/bulk', (c) => {
+app.get('/api/v1/blog/bulk', (c) => {
   return c.text('signup route')
 })
 
