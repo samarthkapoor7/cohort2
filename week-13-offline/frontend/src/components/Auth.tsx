e8dd1e81a6900f06d5a618d64046f80b0cb675cb
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-11-09 18:31:24 +0530

    routes

--- week-13-offline/frontend/src/components/Auth.tsx
+++ week-13-offline/frontend/src/components/Auth.tsx
@@ -1,17 +1,28 @@
 import { SignupInput } from "@samarthkapoor/medium-common";
 import { ChangeEvent, useState } from "react";
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 
 export const Auth = ({ type }: {type: "signup" | "signin" }) => {
+    const navigate = useNavigate();
     const [postInputs, setPostInputs] = useState<SignupInput>({
         name: "",
         username: "",
         password: ""
     });
 
-    function sendRequest() {
-
+    async function sendRequest() {
+        try {
+            const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`, postInputs);
+            const jwt = response.data;
+            localStorage.setItem("token", jwt);
+            navigate("/blogs");
+        } catch(e) {
+            alert("Error while signing up")
+            // alert
+        }
     }
 
     return <div className= "h-screen flex justify-center flex-col">
@@ -38,16 +49,16 @@ export const Auth = ({ type }: {type: "signup" | "signin" }) => {
                     <LabelledInput label="Username" placeholder="samarth@gmail.com" onChange={(e) => {
                         setPostInputs({
                             ...postInputs,
-                            name: e.target.value
+                            username: e.target.value
                         })
                     }} />
                     <LabelledInput label="Password" type={"password"} placeholder="123456" onChange={(e) => {
                         setPostInputs({
                             ...postInputs,
-                            name: e.target.value
+                            password: e.target.value
                         })
                     }} />
-                    <button type="button" className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 mt-8">{type === "signup" ? "Sign up" : "Sign in"}</button>
+                    <button onClick={sendRequest} type="button" className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 mt-8">{type === "signup" ? "Sign up" : "Sign in"}</button>
                 </div>
             </div>
         </div>
