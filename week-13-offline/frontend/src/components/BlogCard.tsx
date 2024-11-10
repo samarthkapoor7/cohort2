c0d91c9e316327a409356123844d3149dff18012
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-11-10 23:18:50 +0530

    completed

--- week-13-offline/frontend/src/components/BlogCard.tsx
+++ week-13-offline/frontend/src/components/BlogCard.tsx
@@ -1,41 +1,46 @@
+import { Link } from "react-router-dom";
 
 interface BlogCardProps {
     authorName: string;
     title: string;
     content: string;
     publishedDate: string;
+    id: number
 }
 export const BlogCard = ({
+    id,
     authorName,
     title,
     content,
     publishedDate
 }: BlogCardProps) => {
-    return <div className="p-4 border-b border-slate-200 pb-4">
-        <div className="flex">
-            <Avatar name={authorName} />
-            <div className="font-extralight pl-2 text-sm flex justify-center flex-col">{authorName}
+    return <Link to={`/blog/${id}`}>
+        <div className="p-4 border-b border-slate-200 pb-4 w-screen max-w-screen-md cursor-pointer">
+            <div className="flex">
+                <Avatar name={authorName} />
+                <div className="font-extralight pl-2 text-sm flex justify-center flex-col">{authorName}
+                </div>
+                <div className="flex justify-center flex-col pl-2">
+                    <Circle />  
+                </div>
+                <div className="pl-2 font-thin text-slate-500 text-sm flex justify-center flex-col">
+                    {publishedDate}
+                </div>
             </div>
-            <div className="flex justify-center flex-col pl-2">
-                <Circle />  
+            <div className="text-xl font-semibold">
+                {title}
             </div>
-            <div className="pl-2 font-thin text-slate-500 text-sm flex justify-center flex-col">
-                {publishedDate}
+            <div className="text-md font-thin">
+                {content.slice(0, 100) + "..."}
+            </div>
+            <div className="text-slate-500 text-sm font-thin pt-4">
+                {`${Math.ceil(content.length / 100)} minute(s) read`}
             </div>
         </div>
-        <div className="text-xl font-semibold">
-            {title}
-        </div>
-        <div className="text-md font-thin">
-            {content.slice(0, 100) + "..."}
-        </div>
-        <div className="text-slate-500 text-sm font-thin pt-4">
-            {`${Math.ceil(content.length / 100)} minute(s) read`}
-        </div>
-    </div>
+    </Link> 
 }
 
-function Circle() {
+export function Circle() {
     return <div className="h-1 w-1 rounded-full bg-slate-500">
 
     </div>
--- week-13-offline/frontend/src/pages/Blogs.tsx
+++ week-13-offline/frontend/src/pages/Blogs.tsx
@@ -1,35 +1,39 @@
 import { Appbar } from "../components/Appbar"
 import { BlogCard } from "../components/BlogCard"
+import { Skeleton } from "../components/Skeletons";
+import { useBlogs } from "../hooks";
 
 export const Blogs = () => {
+    const {loading, blogs} = useBlogs();
+
+    if(loading) {
+        return <div>
+            <Appbar />
+            <div className="flex justify-center">
+                <div>
+                    <Skeleton />
+                    <Skeleton />
+                    <Skeleton />
+                    <Skeleton />
+                    <Skeleton />
+                    <Skeleton />
+                    <Skeleton />
+                </div>
+            </div>
+        </div>
+    }
+
     return <div>
         <Appbar />
         <div className="flex justify-center">
-            <div className="max-w-xl">
-                <BlogCard 
-                    authorName={"Samarth Kapoor"}
-                    title={"How an Ugly Single-Page Website Makes $5000 a month with Affiliate Marketing"}
-                    content={"How an Ugly Single-Page Website Makes $5000 a month with Affiliate Marketing How an Ugly Single-Page Website Makes $5000 a month with Affiliate Marketing"}
+            <div>
+                {blogs.map(blog => <BlogCard 
+                    id={blog.id}
+                    authorName={blog.author.name || "Anonymous"}
+                    title={blog.title}
+                    content={blog.content}
                     publishedDate={"2nd Feb 2024"}
-                    />
-                    <BlogCard 
-                    authorName={"Samarth Kapoor"}
-                    title={"How an Ugly Single-Page Website Makes $5000 a month with Affiliate Marketing"}
-                    content={"How an Ugly Single-Page Website Makes $5000 a month with Affiliate Marketing"}
-                    publishedDate={"2nd Feb 2024"}
-                    />
-                    <BlogCard 
-                    authorName={"Samarth Kapoor"}
-                    title={"How an Ugly Single-Page Website Makes $5000 a month with Affiliate Marketing"}
-                    content={"How an Ugly Single-Page Website Makes $5000 a month with Affiliate Marketing"}
-                    publishedDate={"2nd Feb 2024"}
-                    />
-                    <BlogCard 
-                    authorName={"Samarth Kapoor"}
-                    title={"How an Ugly Single-Page Website Makes $5000 a month with Affiliate Marketing"}
-                    content={"How an Ugly Single-Page Website Makes $5000 a month with Affiliate Marketing"}
-                    publishedDate={"2nd Feb 2024"}
-                    />
+                    />)}
             </div>
         </div>
     </div>
