b9094117dbda43913666c7ec7f981cfd6430cd1f
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-11-12 17:43:40 +0530

    changed file name

--- medium-app/frontend/src/pages/Blog.tsx
+++ medium-app/frontend/src/pages/Blog.tsx
@@ -10,7 +10,7 @@ export const Blog = () => {
         id: id || ""
     });
 
-    if(loading) {
+    if(loading || !blog) {
         return <div>
             <Appbar />
             <div className="h-screen flex flex-col justify-center">
