67de39ed1675f93f467f88dd58ed05b4ce905064
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-12-05 18:12:24 +0530

    mono 2

--- week-16-2/apps/web/app/admin/page.tsx
+++ week-16-2/apps/web/app/admin/page.tsx
@@ -1,8 +1,10 @@
 import { Admin } from "@repo/ui/admin";
+import { InputBox } from "@repo/ui/input-box";
 
 export default function() {
     return <div>
         hi from admin Page
         <Admin />
+        <InputBox />
     </div>
 }
